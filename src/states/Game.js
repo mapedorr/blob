6e4c50842ad228b89d89f772d399f17ee8d004cd
Mateo Robayo <mapedorr@gmail.com>
2016-02-17 18:42:00 -0500

    Solving level bugs. Level 12 recovered.

--- src/states/Game.js
+++ src/states/Game.js
@@ -24,7 +24,7 @@ BasicGame.Game = function (game) {
 };
 
 BasicGame.Game.developmentMode = false;
-BasicGame.currentLevel = 26;
+BasicGame.currentLevel = 12;
 
 BasicGame.Game.prototype.preload = function(){
   this.game.physics.startSystem(Phaser.Physics.ARCADE);
