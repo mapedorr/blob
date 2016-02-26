d079257ef7b6abe9f417f2027781432d65e6b5c4
Mateo Robayo <mapedorr@gmail.com>
2016-02-25 20:20:10 -0500

    Way to fall: Level 29 in process.

--- src/states/Game.js
+++ src/states/Game.js
@@ -24,7 +24,7 @@ BasicGame.Game = function (game) {
 };
 
 BasicGame.Game.developmentMode = false;
-BasicGame.currentLevel = (localStorage.getItem("oh-my-blob") < 30 && localStorage.getItem("oh-my-blob")) || 28;
+BasicGame.currentLevel = (localStorage.getItem("oh-my-blob") < 30 && localStorage.getItem("oh-my-blob")) || 29;
 BasicGame.isRetrying = false;
 
 BasicGame.Game.prototype.preload = function(){
